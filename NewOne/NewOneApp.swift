733cff448ad19e8594306c624028b199b8b4ff58
Shawn Tucker <shqtucke@gmail.com>
2024-05-10 09:58:01 -0400

    Initial Commit

---
+++ NewOne/NewOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewOneApp.swift
+//  NewOne
+//
+//  Created by Shawn Tucker on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
